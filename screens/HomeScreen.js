6ecda676a18db66c56e35bb5604a7b82864eb514
millerb415 <33108827+millerb415@users.noreply.github.com>
2019-01-07 13:34:28 -0800

    eslint config changed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
+
 import React from 'react';
 import {
   StyleSheet,
   Text,
   ImageBackground,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 
 
@@ -22,12 +24,13 @@ export default class HomeScreen extends React.Component {
           source={require("../assets/images/home2.png")}
           resizeMode='cover'
           >
-          <View
-          style={styles.nameStyle}>
-          <Text>
-            GATZBY
-          </Text>
-          </View>
+           <View
+            style={styles.nameStyle}>
+             <Text
+             style={styles.textnameStyle}>
+             GATZBY
+              </Text>
+            </View>
 
           </ImageBackground>
       </View>
@@ -42,9 +45,15 @@ const styles = StyleSheet.create({
     
   },
   nameStyle: {
+    alignSelf: 'center',
+    paddingTop: Dimensions.get('window').height / 9
+  },
+  textnameStyle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white'
   },
   backgroundStyle: {
-  width: '100%', height: '100%'},
-
-  
-});
+  width: '100%', height: '100%'},  
+}
+);
