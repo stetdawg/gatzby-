679130c4b5c4250796db5f4e13b9f649932bce69
millerb415 <33108827+millerb415@users.noreply.github.com>
2019-01-14 12:11:57 -0800

    updated walmart key

--- assets/constants/api_keys.js
+++ assets/constants/api_keys.js
@@ -2,22 +2,14 @@
 // Keys to export (or use internally)
 
 // https://developers.facebook.com/apps/
-export const FACEBOOK_API_KEY = "924171697730805";
+export const FACEBOOK_API_KEY = "";
 
 // https://console.firebase.google.com/
 export const GOOGLE_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAM0afYVkToAnGv3YCOgmwZCGrJjtzYdN4",
-  authDomain: "pricechecker-28e7d.firebaseapp.com",
-  databaseURL: "https://pricechecker-28e7d.firebaseio.com",
-  projectId: "pricechecker-28e7d",
-  storageBucket: "",
-  messagingSenderId: "404859984096"
 };
 
 //https://developer.walmartlabs.com/apps/
-export const WalmartAPI = "kax4864tfwjfumgxk7tk3j78";
+export const WalmartAPI = "hdju6aknz9tm7vmwqn8ucc5r";
 
 export const AmazonAPI = {
-  awsId: "AKIAJIVUHO2V4S64UMCQ",
-  awsSecret: "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"
 };
