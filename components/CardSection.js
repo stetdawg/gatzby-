9555900c828a1bee978046e41bdc8221d79dc815
Micah Edington <micahedington1@yahoo.com>
2019-03-02 03:55:59 -0800

    Reusable Card and Card Section Components.

---
+++ components/CardSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+};
+
+export default CardSection;
