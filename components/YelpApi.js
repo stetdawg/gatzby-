ea2336e39ad3737b09f050ec7bf180ddcc3adaad
RYANSBABY\Ryan <ryanmalcantara@gmail.com>
2019-04-03 02:42:23 -0700

    Moved updated map to testing branch

---
+++ components/YelpApi.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const YELP_API_KEY = 'EuD3hzLX9i9dj5I8_tqsVa1QHV50F8x-xi2MeJzFC1JD-_gV2aPWFXKNgNXywlvWIwl7p0ZZ8p23ECMozUhNJuajYgvCjM9LgAya4DP2vG3XrXd9OBXmouZi0-ujXHYx';
+
+const api = axios.create({
+  baseURL: 'https://api.yelp.com/v3',
+  headers: {
+    Authorization: `Bearer ${YELP_API_KEY}`
+  }
+});
+
+const getStores = userLocation => {
+  return api
+    .get('/businesses/search', {
+      params: {
+        limit: 10,
+        categories: 'computers',
+        ...userLocation
+      }
+    })
+    .then(res =>
+      res.data.businesses.map(business => {
+        return {
+          name: business.name,
+          coords: business.coordinates
+        };
+      })
+    )
+    .catch(error => console.error(error));
+};
+
+export default {
+  getStores
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,105 +1,109 @@
+import YelpService from '../components/YelpApi';
+import Map from '../components/Map';
 import React, { Component } from "react";
 import {     
     AppRegistry,
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    SafeAreaView
 } from 'react-native';
-import MapView from "react-native-maps";
+//import MapView from "react-native-maps";
+import { Location, Permissions } from 'expo';
+
+const region = {
+  latitude: 37.321996988,
+  longitude: -122.0325472123455,
+  latitudeDelta: 0.15,
+  longitudeDelta: 0.15
+}
+
+const deltas = {
+  latitudeDelta: 0.15,
+  longitudeDelta: 0.15
+};
 
 export default class MapScreen extends Component {
-    constructor() {
-        super();
-        this.state = {
-            ready: false,
-            where: {lat: null, lng: null},
-            error: null
-        };
+  state = {
+    region: null,
+    stores: []
+  };
+
+  getStores = async () => {
+    const { latitude, longitude } = this.state.region;
+    const userLocation = { latitude, longitude };
+    const stores = await YelpService.getStores(userLocation);
+    this.setState({ stores });
+  };
+
+  componentWillMount() {
+    this.getLocationAsync();
+  }
+
+  getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      });
     }
-    componentDidMount() {
-        let geoOptions = {
-            enableHighAccuracy: true,
-        };
-        navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoFailure, geoOptions);
-    }
-    geoSuccess = (position) => {
-        //console.log(position.coords.latitude);
-        
-        this.setState({
-            ready: true,
-            where: {lat: position.coords.latitude, lng: position.coords.longitude }
-        });
-    }
-    
-    geoFailure = (err) => {
-        this.setState({error: err.message});
-    }
-    
-    
-    render() {
-        return (
-            <View style={styles.container}>
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: this.state.where.lat,
-                        longitude: this.state.where.lng,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}>
-                        <MapView.Marker
-                            coordinate={{
-                                latitude: Number(this.state.where.lat),
-                                longitude: Number(this.state.where.lng),
-                            }}>
-                                <View style={styles.radius}>
-                                    <View style={styles.marker} />
-                                </View>
-                            </MapView.Marker>
-                    
-                    </MapView>
-                
-            </View>
-        );
-    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    const region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      ...deltas
+    };
+    await this.setState({ region });
+    await this.getStores();
+  }
+
+  render() {
+    const { region, stores } = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        <Map region={region} places={stores} />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    radius: {
-        height: 50,
-        width: 50,
-        borderRadius: 50 / 2,
-        overflow: 'hidden',
-        backgroundColor: 'rgba(0, 122, 255, 0.1)',
-        borderWidth: 1,
-        borderColor: 'rgba(0, 122, 255, 0.3)',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    marker: {
-        height: 20,
-        width: 20,
-        borderWidth: 3,
-        borderColor: 'white',
-        borderRadius: 20 / 2,
-        overflow: 'hidden',
-        backgroundColor: '#007AFF'
-    },
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    big: {
-        fontSize: 48
-    },
-    map: {
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        position: 'absolute'
+  radius: {
+      height: 50,
+      width: 50,
+      borderRadius: 50 / 2,
+      overflow: 'hidden',
+      backgroundColor: 'rgba(0, 122, 255, 0.1)',
+      borderWidth: 1,
+      borderColor: 'rgba(0, 122, 255, 0.3)',
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+  marker: {
+      height: 20,
+      width: 20,
+      borderWidth: 3,
+      borderColor: 'white',
+      borderRadius: 20 / 2,
+      overflow: 'hidden',
+      backgroundColor: '#007AFF'
+  },
+  container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+  big: {
+      fontSize: 48
+  },
+  map: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      position: 'absolute'
 
-    }
+  }
 });
