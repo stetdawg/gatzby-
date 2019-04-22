7f2330534f4419151665f1f3305bb8be5a35353b
RYANSBABY\Ryan <ryanmalcantara@gmail.com>
2019-04-22 05:28:15 -0700

    Flatlist for map, onpress changes coordinates to selected store

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image, Button, Card, Icon, StyleSheet } from 'react-native';
 import { MapView } from 'expo';
+
+import { itemsFetch, savedToResults } from "../actions";
+//import { PRIMARY_COLOR } from "../constants/style";
+import SearchResultsScreen from "../screens/SearchResultsScreen";
+//import Button from '../components/Button';
+//import Card from '../components/Card';
 
 const Marker = MapView.Marker;
 
@@ -11,29 +17,55 @@ export default class Map extends Component {
         key={i}
         title={place.name}
         coordinate={place.coords}
+        description={`${place.addr} ${place.name}`}
+        // onPress={() => {
+        //   this.onMarkerPress.bind(this);
+        // }}
+        onPress={e => console.log(e.nativeEvent)}
       />
     ));
   }
+  
+  onMarkerPress(e) {
+    console.log(e.nativeEvent);
+  }
 
   render() {
-    const { region } = this.props
+    const { region } = this.props;
 
     return (
       <MapView
-      style={styles.container}
+      style={styles.map}
       region={region}
       showsUserLocation
       showsMyLocationButton
       >
         {this.renderMarkers()}
+        
       </MapView>
     );
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     width: '100%',
+    height: '100%',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  map: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    width: '100%',
     height: '100%'
-  }
-};
+
+},
+main: {
+  flex: 1
+}
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -6,10 +6,15 @@ import {
     View,
     Text,
     StyleSheet,
-    SafeAreaView
+    SafeAreaView,
+    FlatList,
+    TouchableOpacity,
+    Image,
+    ListItem,
 } from 'react-native';
-//import MapView from "react-native-maps";
-import { Location, Permissions } from 'expo';
+import { Location, Permissions, Marker } from 'expo';
+//import MapView from 'react-native-elements';
+//import console = require('console');
 
 const region = {
   latitude: 37.321996988,
@@ -22,6 +27,110 @@ const deltas = {
   latitudeDelta: 0.15,
   longitudeDelta: 0.15
 };
+
+  class HorizontalFlatListItem extends Component {
+    render() {
+      return (
+        // <TouchableOpacity 
+        // onPress={() => {
+        // console.log(`lat: ${this.props.item.coords.region}, long: ${this.props.item.coords.longitude}`);
+        // this.setState({region});
+        // // this.map.fitToCoordinates({
+        // //   latitude: 37.321996988,
+        // //   longitude: -122.0325472123455,
+        // //   latitudeDelta: 0.15,
+        // //   longitudeDelta: 0.15
+        // // },
+        // // { edgePadding: { top: 10, right: 10, bottom: 10, left: 10 }, 
+        // // animated: true});
+        // // this.mapView.animateToRegion({
+        // //   latitude: 37.321996988,
+        // //   longitude: -122.0325472123455,
+        // //   latitudeDelta: 0.15,
+        // //   longitudeDelta: 0.15
+        // // }, 1000);
+        // }}>
+        <View 
+          style={{
+            flex: 1,
+            //flexDirectection: 'column',
+            alignItems: 'center',
+            width: 125,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 4,
+          }}>
+          <Text 
+          style={{
+          textAlign: 'center',
+          flex: 1,
+          fontSize: 12,
+          fontWeight: 'bold',
+          //color: 'white',
+          //margin: 20
+          }}>{this.props.item.name}
+          </Text>
+          <Image source={{uri: this.props.item.image}} style={{width: '100%', height: '50%'}} />
+          <Text 
+          style={{
+          textAlign: 'center',
+          flex: 1,
+          fontSize: 12,
+          //margin: 10,
+          //color: 'white',
+          }}>{`${this.props.item.addr} ${this.props.item.city}, ${this.props.item.state1} ${this.props.item.zip}`}
+          </Text>
+          <Text 
+          style={{
+          textAlign: 'center',
+          flex: 1,
+          fontSize: 12,
+          margin: 10,
+          //color: 'white',
+          }}>{this.props.item.phone}
+          </Text>
+
+        </View>
+        //</TouchableOpacity>
+      );
+    }
+  }
+
+  // class Map extends Component {
+  //   renderMarkers() {
+  //     return this.props.places.map((place, i) => (
+  //       <MapView.Marker 
+  //         key={i}
+  //         title={place.name}
+  //         coordinate={place.coords}
+  //         description={`${place.addr} ${place.name}`}
+  //       />
+  //     ));
+  //   }
+    
+  //   render() {
+  //     const { region } = this.props;
+  
+  //     return (
+  //       <MapView
+  //       style={{ 
+  //         left: 0,
+  //         right: 0,
+  //         top: 0,
+  //         bottom: 0,
+  //         position: 'absolute',
+  //         width: '100%',
+  //         height: '100%'}}
+  //       region={region}
+  //       showsUserLocation
+  //       showsMyLocationButton
+  //       >
+  //         {this.renderMarkers()}
+  //       </MapView>
+  //     );
+  //   }
+  // }
 
 export default class MapScreen extends Component {
   state = {
@@ -61,9 +170,69 @@ export default class MapScreen extends Component {
   render() {
     const { region, stores } = this.state;
     return (
-      <SafeAreaView style={styles.container}>
-        <Map region={region} places={stores} />
-      </SafeAreaView>
+      <View style={styles.main}>
+        <View style={styles.container}>
+          <Map region={region} places={stores} />
+        </View>
+         <FlatList
+          horizontal={true}
+          data={this.state.stores}
+          //renderRow={this.onMarkerPress.bind(this)}
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+            onPress={(e) => {
+            console.log(`lat: ${item.coords.latitude}, long: ${item.coords.longitude}`);
+            this.state.region = {
+              latitude: item.coords.latitude,
+              longitude: item.coords.longitude,
+              latitudeDelta: 0.15,
+              longitudeDelta: 0.15
+            };
+            this.setState({
+              latitude: item.coords.latitude,
+              longitude: item.coords.longitude,
+              latitudeDelta: 0.15,
+              longitudeDelta: 0.15
+            });
+            console.log({region});
+            //console.log(e.nativeEvent.coordinate);
+            //console.log(e.nativeEvent.key.showCallout());
+            //this.map.animateToRegion(region, 100);
+            //this.animateEvent(region);
+            // this.setState({
+            //   latitude: 37.321996988,
+            //   longitude: -122.0325472123455
+            // });
+            // this.map.animateToRegion({
+            //   latitude: item.coords.latitude,
+            //   longitude: item.coords.longitude,
+            //   latitudeDelta: 0.15,
+            //   longitudeDelta: 0.15
+            // }, 100);
+            // this.map.fitToCoordinates({
+            //   latitude: 37.321996988,
+            //   longitude: -122.0325472123455,
+            //   latitudeDelta: 0.15,
+            //   longitudeDelta: 0.15
+            // },
+            // { edgePadding: { top: 10, right: 10, bottom: 10, left: 10 }, 
+            // animated: true});
+            // this.mapView.animateToRegion({
+            //   latitude: 37.321996988,
+            //   longitude: -122.0325472123455,
+            //   latitudeDelta: 0.15,
+            //   longitudeDelta: 0.15
+            // }, 1000);
+            }}>
+                <HorizontalFlatListItem item={item}>
+                
+                </HorizontalFlatListItem>
+              
+              </TouchableOpacity>             
+          )}
+          keyExtractor={item => item.id}
+        /> 
+      </View>
     );
   }
 }
@@ -90,20 +259,33 @@ const styles = StyleSheet.create({
       backgroundColor: '#007AFF'
   },
   container: {
+      width: '100%',
+      height: '100%',
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center'
+      //paddingBottom: 50
+      // justifyContent: 'center',
+      // overflow: 'hidden'
+      // position: 'absolute',
+      // top: 0,
+      // left: 0,
+      // right: 0,
+      // bottom: 0
   },
   big: {
       fontSize: 48
   },
-  map: {
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      position: 'absolute'
-
+  // map: {
+  //     left: 0,
+  //     right: 0,
+  //     top: 0,
+  //     bottom: 0,
+  //     position: 'absolute',
+  //     width: 200,
+  //     height: 200
+  // },
+  main: {
+    flex: 1
   }
 });
